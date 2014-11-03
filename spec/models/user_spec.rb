a03590f6a21ab10d8add6304f3e9871ad53431d7
liyijie <liyijie825@gmail.com>
2014-11-03 10:44:01 +0800

    add user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    before(:each) do
+
+    end
+    it "should have a phone method" do
+      
+    end
+  end
 end
