b36220862925032d8ae2983245b3dd360fee70b4
liyijie <liyijie825@gmail.com>
2015-03-14 20:38:15 +0800

    add sms token generator

--- app/controllers/sms_tokens_controller.rb
+++ app/controllers/sms_tokens_controller.rb
@@ -3,6 +3,21 @@ class SmsTokensController < ApplicationController
   end
 
   def create
-    
+    sms_token = SmsToken.new sms_token_params
+    token = (0..9).to_a.sample(6).join
+
+    # 发送短信
+    sms_token.token = token
+    sms_token.save
   end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+    def sms_token_params
+      params.require(:sms_token).permit(
+        :phone
+        )
+    end
+
 end
